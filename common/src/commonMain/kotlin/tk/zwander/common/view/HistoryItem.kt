241dd477b47a60a646d60a72dd1770c4ef73ef2c
Zachary Wander <zachary.wander@gmail.com>
2021-03-03 14:47:22 -0500

    Redo HistoryItem layout

--- common/src/commonMain/kotlin/tk/zwander/common/view/HistoryItem.kt
+++ common/src/commonMain/kotlin/tk/zwander/common/view/HistoryItem.kt
@@ -2,11 +2,16 @@ package tk.zwander.common.view
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import tk.zwander.common.data.HistoryInfo
 
 @Composable
@@ -23,24 +28,23 @@ fun HistoryItem(
             Row(
                 modifier = Modifier.fillMaxWidth()
             ) {
-                OutlinedTextField(
-                    value = info.date.format("MMM dd, yyyy"),
-                    onValueChange = {},
-                    readOnly = true,
-                    modifier = Modifier.weight(1f),
-                    label = { Text("Date") },
-                    singleLine = true
+                Text(
+                    text = info.date.format("MMM dd, yyyy"),
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.align(Alignment.CenterVertically),
+                    fontSize = 20.sp,
+                    lineHeight = 20.sp
                 )
 
                 Spacer(Modifier.width(8.dp))
 
-                OutlinedTextField(
-                    value = info.androidVersion,
-                    onValueChange = {},
-                    readOnly = true,
-                    modifier = Modifier.weight(1f),
-                    label = { Text("Android Version") },
-                    singleLine = true
+                Text(
+                    text = "Android ${info.androidVersion}",
+                    fontSize = 16.sp,
+                    modifier = Modifier.align(Alignment.Bottom)
+                        .offset(y = (-1).dp),
+                    color = MaterialTheme.typography.body1.color,
+                    lineHeight = 20.sp
                 )
             }
 
