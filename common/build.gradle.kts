9b4255bdf9fc7e741dd29691fa47fdb54e4823e6
Zachary Wander <zachary.wander@gmail.com>
2021-06-25 12:24:50 -0400

    Update dependencies

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("multiplatform")
-    id("org.jetbrains.compose") version "0.4.0-build188"
     id("com.android.library")
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
     id("com.github.gmazzo.buildconfig") version "3.0.0"
 }
 
@@ -13,34 +13,6 @@ repositories {
     mavenCentral()
 }
 
-android {
-    compileSdkVersion(29)
-    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-    sourceSets["main"].resources.srcDir("src/commonMain/resources")
-    defaultConfig {
-        minSdkVersion(24)
-        targetSdkVersion(29)
-
-        versionCode(project.properties["versionCode"].toString().toInt())
-        versionName(version.toString())
-    }
-
-    configurations {
-        create("androidTestApi")
-        create("androidTestDebugApi")
-        create("androidTestReleaseApi")
-        create("testApi")
-        create("testDebugApi")
-        create("testReleaseApi")
-    }
-}
-
-buildConfig {
-    className("GradleConfig")
-    buildConfigField("String", "versionName", "\"${project.properties["versionName"]}\"")
-    buildConfigField("String", "versionCode", "\"${project.properties["versionCode"]}\"")
-}
-
 kotlin {
     android()
 
@@ -49,16 +21,16 @@ kotlin {
             kotlinOptions.jvmTarget = "11"
         }
     }
-    
+
     sourceSets {
-        val commonMain by getting {
+        named("commonMain") {
             dependencies {
                 api(compose.runtime)
                 api(compose.foundation)
                 api(compose.material)
                 api(compose.ui)
 
-                api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
+                api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
                 api("org.jetbrains.kotlinx:kotlinx-datetime:0.1.1")
                 api("org.jetbrains.kotlinx:kotlinx-io-jvm:0.1.16")
 
@@ -73,33 +45,46 @@ kotlin {
             }
         }
 
-        val desktopMain by getting {
+        named("desktopMain") {
             dependencies {
                 api(compose.runtime)
                 api(compose.foundation)
                 api(compose.material)
                 api(compose.ui)
 
-                api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
+                api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
                 api("org.jsoup:jsoup:1.13.1")
             }
         }
 
-        val androidMain by getting {
+        named("androidMain") {
             dependencies {
                 api(compose.runtime)
                 api(compose.foundation)
                 api(compose.material)
                 api(compose.ui)
 
-                api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
-                api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.3")
+                api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
+                api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0")
                 api("org.jsoup:jsoup:1.13.1")
 
-                api("androidx.appcompat:appcompat:1.3.0-rc01")
-                api("androidx.core:core-ktx:1.3.2")
+                api("androidx.appcompat:appcompat:1.4.0-alpha02")
+                api("androidx.core:core-ktx:1.5.0")
                 api("androidx.documentfile:documentfile:1.0.1")
             }
         }
     }
+}
+
+android {
+    compileSdk = 29
+
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    sourceSets["main"].resources.srcDir("src/commonMain/resources")
+}
+
+buildConfig {
+    className("GradleConfig")
+    buildConfigField("String", "versionName", "\"${project.properties["versionName"]}\"")
+    buildConfigField("String", "versionCode", "\"${project.properties["versionCode"]}\"")
 }
--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
     kotlin("multiplatform")
-    id("org.jetbrains.compose") version "0.4.0-build188"
+    id("org.jetbrains.compose")
 }
 
 group = "tk.zwander"
