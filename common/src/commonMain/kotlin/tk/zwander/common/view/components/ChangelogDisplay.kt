5f10b280cf09d854b1070fd843fce67d4ce2fa76
Zachary Wander <zachary.wander@gmail.com>
2021-07-10 14:17:44 -0400

    Give ChangelogDisplay a border

--- common/src/commonMain/kotlin/tk/zwander/common/view/components/ChangelogDisplay.kt
+++ common/src/commonMain/kotlin/tk/zwander/common/view/components/ChangelogDisplay.kt
@@ -1,10 +1,15 @@
 package tk.zwander.common.view.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import tk.zwander.common.data.changelog.Changelog
@@ -15,31 +20,39 @@ import tk.zwander.common.util.parseHtml
 fun ChangelogDisplay(
     changelog: Changelog
 ) {
-    Column(
-        modifier = Modifier.fillMaxWidth()
+    Card(
+        backgroundColor = Color.Transparent,
+        elevation = 0.dp,
+        border = BorderStroke(1.dp, Color(255, 255, 255, 100)),
+        modifier = Modifier.padding(4.dp)
     ) {
-        if (changelog.secPatch != null) {
-            Text(
-                fontWeight = FontWeight.Bold,
-                text = "Security: ${changelog.secPatch}",
-                modifier = Modifier.fillMaxWidth()
-            )
+        Column(
+            modifier = Modifier.fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            if (changelog.secPatch != null) {
+                Text(
+                    fontWeight = FontWeight.Bold,
+                    text = "Security: ${changelog.secPatch}",
+                    modifier = Modifier.fillMaxWidth()
+                )
 
-            Spacer(Modifier.height(8.dp))
-        }
+                Spacer(Modifier.height(8.dp))
+            }
 
-        if (changelog.relDate != null) {
-            Text(
-                fontWeight = FontWeight.Bold,
-                text = "Release: ${changelog.relDate}",
-                modifier = Modifier.fillMaxWidth()
-            )
+            if (changelog.relDate != null) {
+                Text(
+                    fontWeight = FontWeight.Bold,
+                    text = "Release: ${changelog.relDate}",
+                    modifier = Modifier.fillMaxWidth()
+                )
 
-            Spacer(Modifier.height(8.dp))
-        }
+                Spacer(Modifier.height(8.dp))
+            }
 
-        if (changelog.notes != null) {
-            Text(changelog.notes.parseHtml())
+            if (changelog.notes != null) {
+                Text(changelog.notes.parseHtml())
+            }
         }
     }
 }
