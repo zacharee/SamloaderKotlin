f729986deff8d508edbb63e418c494cc98495443
Zachary Wander <zachary.wander@gmail.com>
2021-07-10 14:10:20 -0400

    Change corner radii

--- common/src/commonMain/kotlin/tk/zwander/common/view/components/CustomMaterialTheme.kt
+++ common/src/commonMain/kotlin/tk/zwander/common/view/components/CustomMaterialTheme.kt
@@ -1,9 +1,14 @@
 package tk.zwander.common.view.components
 
+import androidx.compose.foundation.shape.CornerBasedShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Shapes
 import androidx.compose.material.darkColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import tk.zwander.common.data.*
 
 /**
@@ -18,6 +23,11 @@ fun CustomMaterialTheme(block: @Composable()() -> Unit) {
             secondary = Color(secondary.toLong(16)),
             secondaryVariant = Color(secondaryVariant.toLong(16)),
             background = Color(background.toLong(16))
+        ),
+        shapes = Shapes(
+            small = RoundedCornerShape(8.dp),
+            medium = RoundedCornerShape(12.dp),
+            large = RoundedCornerShape(16.dp)
         )
     ) {
         block()
