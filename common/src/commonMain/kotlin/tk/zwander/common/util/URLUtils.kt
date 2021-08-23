e6057cb215be8994d3f3efc5eb4acc566b573b23
Zachary Wander <zachary.wander@gmail.com>
2021-08-23 12:59:38 -0400

    Use proper CORS proxy URL

--- common/src/commonMain/kotlin/tk/zwander/common/util/URLUtils.kt
+++ common/src/commonMain/kotlin/tk/zwander/common/util/URLUtils.kt
@@ -1,5 +1,5 @@
 package tk.zwander.common.util
 
 fun generateProperUrl(useProxy: Boolean, url: String): String {
-    return "${if (useProxy) "http://localhost:8080/" else ""}$url"
+    return "${if (useProxy) "https://cors-proxy-zwander.herokuapp.com/" else ""}$url"
 }
