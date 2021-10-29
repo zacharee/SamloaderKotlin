35f8574be509c0df50de00163a652fafb72debff
Zachary Wander <zachary.wander@gmail.com>
2021-10-28 23:24:26 -0400

    Fix History view

--- common/src/commonMain/kotlin/tk/zwander/common/util/Firmware.kt
+++ common/src/commonMain/kotlin/tk/zwander/common/util/Firmware.kt
@@ -28,6 +28,7 @@ suspend fun getFirmwareHistoryString(model: String, region: String): String? {
     }
 
     val response = client.get<HttpResponse> {
+        userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36")
         url(origUrl)
     }
 
--- commonCompose/src/desktopMain/kotlin/tk/zwander/commonCompose/view/pages/HistoryView.kt
+++ commonCompose/src/desktopMain/kotlin/tk/zwander/commonCompose/view/pages/HistoryView.kt
@@ -9,15 +9,16 @@ actual object PlatformHistoryView {
     actual suspend fun parseHistory(body: String): List<HistoryInfo> {
         val doc = Jsoup.parse(body)
 
-        val table = doc.selectFirst("#flist")
-        val rows = table!!.children()[0].children().run { subList(1, size) }
+        val listItems = doc.select(".index_list").apply {
+            removeAt(0)
+        }
 
-        return rows.map {
-            val cols = it.select("td")
-            val date = cols[5].text()
-            val version = cols[4].text()
+        return listItems.map {
+            val cols = it.select(".index_body_list")
+            val date = cols[6].text()
+            val version = cols[5].text()
 
-            val link = cols[7].children().attr("href")
+            val link = cols[0].children()[0].children().attr("href")
             val split = link.split("-")
 
             val pda = split[split.lastIndex - 1]
