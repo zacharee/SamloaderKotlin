2d36e1edaefd0ca0e514911e152347315d063313
Zachary Wander <zachary.wander@gmail.com>
2021-12-04 11:42:32 -0500

    Update dependencies

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     id("com.android.library")
     kotlin("multiplatform")
     id("org.jetbrains.compose")
-    id("com.github.gmazzo.buildconfig") version "3.0.2"
+    id("com.github.gmazzo.buildconfig") version "3.0.3"
 }
 
 group = "tk.zwander"
@@ -50,17 +50,17 @@ kotlin {
                 api(compose.runtime)
 
                 api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
-                api("org.jetbrains.kotlinx:kotlinx-datetime:0.3.0")
+                api("org.jetbrains.kotlinx:kotlinx-datetime:0.3.1")
 //                api("org.jetbrains.kotlinx:kotlinx-io:0.1.16")
                 api("com.squareup.okio:okio-multiplatform:3.0.0-alpha.9")
 
-                api("com.soywiz.korlibs.krypto:krypto:2.4.6")
-                api("com.soywiz.korlibs.korio:korio:2.4.6")
-                api("com.soywiz.korlibs.klock:klock:2.4.6")
+                api("com.soywiz.korlibs.krypto:krypto:2.4.8")
+                api("com.soywiz.korlibs.korio:korio:2.4.8")
+                api("com.soywiz.korlibs.klock:klock:2.4.8")
                 api("co.touchlab:stately-common:1.2.0-nmm")
                 api("co.touchlab:stately-isolate:1.2.0-nmm")
-                api("io.ktor:ktor-client-core:1.6.4")
-                api("io.ktor:ktor-client-auth:1.6.4")
+                api("io.ktor:ktor-client-core:1.6.6")
+                api("io.ktor:ktor-client-auth:1.6.6")
                 api("io.fluidsonic.i18n:fluid-i18n:0.10.0")
                 api("io.fluidsonic.country:fluid-country:0.10.0")
             }
@@ -70,7 +70,7 @@ kotlin {
             dependencies {
                 api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
                 api("org.jsoup:jsoup:1.14.3")
-                api("io.ktor:ktor-client-cio:1.6.4")
+                api("io.ktor:ktor-client-cio:1.6.6")
             }
         }
 
@@ -80,19 +80,18 @@ kotlin {
                 api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
                 api("org.jsoup:jsoup:1.14.3")
 
-                api("androidx.appcompat:appcompat:1.4.0-rc01")
+                api("androidx.appcompat:appcompat:1.4.0")
                 api("androidx.core:core-ktx:1.7.0")
                 api("androidx.documentfile:documentfile:1.1.0-alpha01")
-                api("io.ktor:ktor-client-cio:1.6.4")
+                api("io.ktor:ktor-client-cio:1.6.6")
             }
         }
 
         named("jsMain") {
             dependencies {
                 api(compose.web.core)
-                api(compose.web.widgets)
 
-                api("io.ktor:ktor-client-js:1.6.4")
+                api("io.ktor:ktor-client-js:1.6.6")
 
                 api("org.jetbrains.kotlin-wrappers:kotlin-react:17.0.2-pre.258-kotlin-1.5.31")
                 api("org.jetbrains.kotlin-wrappers:kotlin-react-dom:17.0.2-pre.258-kotlin-1.5.31")
