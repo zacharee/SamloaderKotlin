dab46c21bcbf3244670477413827f91e18e6af2d
Zachary Wander <zachary.wander@gmail.com>
2022-06-17 15:28:28 -0400

    Update dependencies

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -63,7 +63,7 @@ kotlin {
                 api(compose.runtime)
 
                 api("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${rootProject.extra["kotlinVersion"]}")
-                api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+                api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
                 api("org.jetbrains.kotlinx:kotlinx-datetime:0.3.2")
                 api("com.squareup.okio:okio-multiplatform:3.0.0-alpha.9")
 
@@ -95,19 +95,19 @@ kotlin {
 
         named("androidMain") {
             dependencies {
-                api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
+                api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.2")
                 api("org.jsoup:jsoup:$jsoupVersion")
 
-                api("androidx.appcompat:appcompat:1.4.1")
+                api("androidx.appcompat:appcompat:1.4.2")
                 api("androidx.fragment:fragment-ktx:1.4.1")
                 api("androidx.activity:activity-compose:1.4.0")
-                api("androidx.core:core-ktx:1.7.0")
+                api("androidx.core:core-ktx:1.8.0")
                 api("androidx.documentfile:documentfile:1.1.0-alpha01")
                 api("io.ktor:ktor-client-cio:$ktorVersion")
                 api("de.comahe.i18n4k:i18n4k-core-jvm:0.4.0")
 
                 // Remove this once JB Compose gets the updated version.
-                api("androidx.compose.foundation:foundation-layout:1.2.0-beta01")
+                api("androidx.compose.foundation:foundation-layout:1.2.0-rc01")
             }
         }
 
