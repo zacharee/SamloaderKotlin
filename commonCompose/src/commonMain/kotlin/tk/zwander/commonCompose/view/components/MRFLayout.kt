3f4a37cf7b3af4669a2ce3e23b601e0bdb78b5fa
Zachary Wander <zachary.wander@gmail.com>
2022-07-25 21:56:08 -0400

    Create dedicated SplitComponent

--- commonCompose/src/commonMain/kotlin/tk/zwander/commonCompose/view/components/MRFLayout.kt
+++ commonCompose/src/commonMain/kotlin/tk/zwander/commonCompose/view/components/MRFLayout.kt
@@ -32,103 +32,45 @@ import tk.zwander.samloaderkotlin.strings
  */
 @Composable
 fun MRFLayout(model: BaseModel, canChangeOption: Boolean, canChangeFirmware: Boolean, showFirmware: Boolean = true) {
-    BoxWithConstraints(
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        val density = LocalDensity.current
-        val constraint = rememberIsOverScaledThreshold(constraints.maxWidth)
-        val constraints = this.constraints
-
-        val padding = remember(density) { with(density) { 4.dp.toPx() } }
-
-        var fieldHeight by remember(density) { mutableStateOf(0) }
-        val layoutHeight by animateIntAsState(if (constraint) fieldHeight else (2 * fieldHeight + 2 * padding).toInt())
-
-        val modelFieldWidth by derivedStateOf { constraints.maxWidth * 0.6 - padding }
-        val regionFieldWidth by derivedStateOf { constraints.maxWidth * 0.4 - padding }
-
-        val transition = updateTransition(constraint)
-
-        val regionFieldOffset by transition.animateIntOffset {
-            IntOffset(
-                x = if (it) (modelFieldWidth + (padding * 2)).toInt() else 0,
-                y = if (it) 0 else (fieldHeight + 2 * padding).toInt()
+    SplitComponent(
+        startComponent = {
+            OutlinedTextField(
+                value = model.model,
+                onValueChange = {
+                    model.model = it.uppercase().trim()
+                    if ((model is DownloadModel && !model.manual)) {
+                        model.fw = ""
+                        model.osCode = ""
+                    }
+                },
+                modifier = Modifier.layoutId("model"),
+                label = { Text(strings.modelHint()) },
+                readOnly = !canChangeOption,
+                keyboardOptions = KeyboardOptions(KeyboardCapitalization.Characters),
+                singleLine = true
             )
-        }
-        val modelFieldAnimWidth by transition.animateInt {
-            if (it) modelFieldWidth.toInt() else constraints.maxWidth
-        }
-        val regionFieldAnimWidth by transition.animateInt {
-            if (constraint) regionFieldWidth.toInt() else constraints.maxWidth
-        }
-
-        val lHeight by rememberUpdatedState(layoutHeight)
-        val rOffset by rememberUpdatedState(regionFieldOffset)
-        val mWidth by rememberUpdatedState(modelFieldAnimWidth)
-        val rWidth by rememberUpdatedState(regionFieldAnimWidth)
-
-        MultiMeasureLayout(
-            modifier = Modifier.fillMaxWidth(),
-            content = {
-                OutlinedTextField(
-                    value = model.model,
-                    onValueChange = {
-                        model.model = it.uppercase().trim()
-                        if ((model is DownloadModel && !model.manual)) {
-                            model.fw = ""
-                            model.osCode = ""
-                        }
-                    },
-                    modifier = Modifier.layoutId("model"),
-                    label = { Text(strings.modelHint()) },
-                    readOnly = !canChangeOption,
-                    keyboardOptions = KeyboardOptions(KeyboardCapitalization.Characters),
-                    singleLine = true
-                )
-
-                OutlinedTextField(
-                    value = model.region,
-                    onValueChange = {
-                        model.region = it.uppercase().trim()
-                        if ((model is DownloadModel && !model.manual)) {
-                            model.fw = ""
-                            model.osCode = ""
-                        }
-                    },
-                    modifier = Modifier.layoutId("region"),
-                    label = { Text(strings.regionHint()) },
-                    readOnly = !canChangeOption,
-                    keyboardOptions = KeyboardOptions(KeyboardCapitalization.Characters),
-                    singleLine = true
-                )
-            }
-        ) { measurables, c ->
-            if (fieldHeight == 0) {
-                fieldHeight = measurables[0].measure(constraints).height
-            }
-
-            layout(c.maxWidth, lHeight) {
-                measurables.forEachIndexed { index, measurable ->
-                    val placeable = measurable.measure(
-                        Constraints(
-                            maxWidth = when (index) {
-                                0 -> mWidth
-                                else -> rWidth
-                            },
-                            minWidth = when (index) {
-                                0 -> mWidth
-                                else -> rWidth
-                            }
-                        )
-                    )
-
-                    placeable.place(
-                        if (index == 0) IntOffset.Zero else rOffset
-                    )
-                }
-            }
-        }
-    }
+        },
+        endComponent = {
+            OutlinedTextField(
+                value = model.region,
+                onValueChange = {
+                    model.region = it.uppercase().trim()
+                    if ((model is DownloadModel && !model.manual)) {
+                        model.fw = ""
+                        model.osCode = ""
+                    }
+                },
+                modifier = Modifier.layoutId("region"),
+                label = { Text(strings.regionHint()) },
+                readOnly = !canChangeOption,
+                keyboardOptions = KeyboardOptions(KeyboardCapitalization.Characters),
+                singleLine = true
+            )
+        },
+        startRatio = 0.6,
+        endRatio = 0.4,
+        modifier = Modifier.fillMaxWidth()
+    )
 
     if (showFirmware) {
         Spacer(Modifier.size(8.dp))
---
+++ commonCompose/src/commonMain/kotlin/tk/zwander/commonCompose/view/components/SplitComponent.kt
@@ -0,0 +1,91 @@
+package tk.zwander.commonCompose.view.components
+
+import androidx.compose.animation.core.animateInt
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.animation.core.animateIntOffset
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SplitComponent(
+    startComponent: @Composable () -> Unit,
+    endComponent: @Composable () -> Unit,
+    threshold: Dp = 400.dp,
+    modifier: Modifier = Modifier,
+    startRatio: Double = 0.5,
+    endRatio: Double = 0.5,
+    horizontalSpacing: Dp = 8.dp,
+    verticalSpacing: Dp = 8.dp,
+) {
+    BoxWithConstraints(
+        modifier = modifier
+    ) {
+        val constraints = this.constraints
+        val density = LocalDensity.current
+
+        val (horizontalSpacingPx, verticalSpacingPx) = with(density) {
+            horizontalSpacing.toPx() to verticalSpacing.toPx()
+        }
+
+        val isOverThreshold = remember(density, constraints.maxWidth) {
+            (constraints.maxWidth / (density.density * density.fontScale)).dp > threshold
+        }
+        val transition = updateTransition(isOverThreshold)
+
+        var fieldHeight by remember(density) { mutableStateOf(0) }
+
+        val layoutHeight by animateIntAsState(if (isOverThreshold) fieldHeight else (2 * fieldHeight + verticalSpacingPx).toInt())
+
+        val startComponentWidth by derivedStateOf { constraints.maxWidth * startRatio - (horizontalSpacingPx / 2.0) }
+        val endComponentWidth by derivedStateOf { constraints.maxWidth * endRatio - (horizontalSpacingPx / 2.0) }
+
+        val endComponentOffset by transition.animateIntOffset {
+            IntOffset(
+                x = if (it) (startComponentWidth + horizontalSpacingPx).toInt() else 0,
+                y = if (it) 0 else (fieldHeight + verticalSpacingPx).toInt()
+            )
+        }
+
+        val startComponentAnimWidth by transition.animateInt {
+            if (it) startComponentWidth.toInt() else constraints.maxWidth
+        }
+        val endComponentAnimWidth by transition.animateInt {
+            if (it) endComponentWidth.toInt() else constraints.maxWidth
+        }
+
+        val lHeight by rememberUpdatedState(layoutHeight)
+        val eOffset by rememberUpdatedState(endComponentOffset)
+        val sWidth by rememberUpdatedState(startComponentAnimWidth)
+        val eWidth by rememberUpdatedState(endComponentAnimWidth)
+
+        Layout(
+            content = {
+                startComponent()
+                endComponent()
+            }
+        ) { measurables, c ->
+            layout(c.maxWidth, lHeight) {
+                measurables.forEachIndexed { index, measurable ->
+                    val placeable = measurable.measure(
+                        Constraints(
+                            maxWidth = if (index == 0) sWidth else eWidth,
+                            minWidth = if (index == 0) sWidth else eWidth
+                        )
+                    )
+
+                    fieldHeight = placeable.height
+
+                    placeable.place(if (index == 0) IntOffset.Zero else eOffset)
+                }
+            }
+        }
+    }
+}
