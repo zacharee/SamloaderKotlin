3b80016b95c399f44022c9a6d7fc36eae57a3ac7
Zachary Wander <zachary.wander@gmail.com>
2023-11-12 14:58:00 -0500

    Fix old code error

--- android/src/main/java/tk/zwander/samsungfirmwaredownloader/DownloaderService.kt
+++ android/src/main/java/tk/zwander/samsungfirmwaredownloader/DownloaderService.kt
@@ -82,7 +82,7 @@ class DownloaderService : Service() {
                             PendingIntent.getActivity(
                                 this, 101,
                                 Intent(this, MainActivity::class.java),
-                                PendingIntent.FLAG_UPDATE_CURRENT or (1 shl 25)
+                                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE,
                             )
                         )
                         .build()
