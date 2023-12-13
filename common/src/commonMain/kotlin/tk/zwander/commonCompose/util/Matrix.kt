8e54baa396a73dd1cc86356a2e2c08a7f12fd6ff
Zachary Wander <zachary.wander@gmail.com>
2023-12-13 18:40:08 -0500

    Remove unused classes

--- common/src/commonMain/kotlin/tk/zwander/commonCompose/util/Matrix.kt
+++
@@ -1,7 +0,0 @@
-package tk.zwander.commonCompose.util
-
-expect class Matrix() {
-    fun preRotate(degrees: Float, px: Float, py: Float): Boolean
-    fun preScale(sx: Float, sy: Float, px: Float, py: Float): Boolean
-    fun mapPoints(pts: FloatArray)
-}
