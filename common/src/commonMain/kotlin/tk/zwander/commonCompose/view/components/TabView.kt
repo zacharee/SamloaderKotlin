341f54f849466131c113cf35ad17fcbb0e580986
Zachary Wander <zachary.wander@gmail.com>
2024-01-24 17:08:09 -0500

    Iconify tab buttons when text would ellipsize

--- common/src/commonMain/kotlin/tk/zwander/commonCompose/view/components/TabView.kt
+++ common/src/commonMain/kotlin/tk/zwander/commonCompose/view/components/TabView.kt
@@ -2,18 +2,26 @@
 
 package tk.zwander.commonCompose.view.components
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import dev.icerock.moko.resources.ImageResource
 import dev.icerock.moko.resources.StringResource
+import dev.icerock.moko.resources.compose.painterResource
+import dev.icerock.moko.resources.compose.stringResource
 import io.ktor.utils.io.core.internal.DangerousInternalIoApi
 import korlibs.io.annotations.Keep
-import tk.zwander.common.util.invoke
 import tk.zwander.commonCompose.view.LocalUseMicaEffect
 import tk.zwander.commonCompose.view.pages.DecryptView
 import tk.zwander.commonCompose.view.pages.DownloadView
@@ -32,26 +40,37 @@ val pages = Page::class.sealedSubclasses
 sealed class Page(
     val render: @Composable () -> Unit,
     val labelRes: StringResource,
+    val iconRes: ImageResource,
     val index: Int,
 ) {
     @Keep
-    data object Downloader : Page({ DownloadView() }, MR.strings.downloader, 0)
+    data object Downloader : Page({ DownloadView() }, MR.strings.downloader, MR.images.download, 0)
+
     @Keep
-    data object Decrypter : Page({ DecryptView() }, MR.strings.decrypter, 1)
+    data object Decrypter :
+        Page({ DecryptView() }, MR.strings.decrypter, MR.images.lock_open_outline, 1)
+
     @Keep
-    data object History : Page({ HistoryView() }, MR.strings.history, 2)
+    data object History : Page({ HistoryView() }, MR.strings.history, MR.images.history, 2)
+
     @Keep
-    data object SettingsAbout : Page({ SettingsAboutView() }, MR.strings.more, 3)
+    data object SettingsAbout : Page({ SettingsAboutView() }, MR.strings.more, MR.images.cog, 3)
 }
 
 /**
  * Allows the user to switch among the different app pages.
  */
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 internal fun TabView(
     selectedPage: Int,
     onPageSelected: (Int) -> Unit,
 ) {
+    val density = LocalDensity.current
+    var anyEllipsis by remember(density.density, density.fontScale) {
+        mutableStateOf(false)
+    }
+
     TabRow(
         modifier = Modifier.fillMaxWidth()
             .height(48.dp),
@@ -60,15 +79,34 @@ internal fun TabView(
         containerColor = if (LocalUseMicaEffect.current) Color.Transparent else TabRowDefaults.containerColor,
     ) {
         pages.forEach { page ->
-            Tab(
-                selected = selectedPage == page.index,
-                text = {
+            val text: (@Composable () -> Unit)? = if (anyEllipsis) null else {
+                {
                     Text(
-                        text = page.labelRes(),
+                        text = stringResource(page.labelRes),
                         overflow = TextOverflow.Ellipsis,
                         maxLines = 1,
+                        onTextLayout = {
+                            if (!anyEllipsis) {
+                                anyEllipsis = it.isLineEllipsized(0)
+                            }
+                        },
                     )
-                },
+                }
+            }
+
+            val icon: (@Composable () -> Unit)? = if (anyEllipsis) {
+                {
+                    Icon(
+                        painter = painterResource(page.iconRes),
+                        contentDescription = stringResource(page.labelRes),
+                    )
+                }
+            } else null
+
+            Tab(
+                selected = selectedPage == page.index,
+                text = text,
+                icon = icon,
                 onClick = {
                     onPageSelected(page.index)
                 },
