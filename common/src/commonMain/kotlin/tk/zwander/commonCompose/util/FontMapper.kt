42a14ad2b3d14b1154cd8ec21e6eb4f418fe14c2
Zachary Wander <zachary.wander@gmail.com>
2024-04-28 17:27:24 -0400

    Remove FontMapper

--- common/src/commonMain/kotlin/tk/zwander/commonCompose/util/FontMapper.kt
+++
@@ -1,9 +0,0 @@
-package tk.zwander.commonCompose.util
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.font.FontFamily
-
-expect object FontMapper {
-    @Composable
-    fun mapGenericFontFamilyToSpecificFontFamily(family: FontFamily): FontFamily
-}
--- common/src/commonMain/kotlin/tk/zwander/commonCompose/view/components/CustomMaterialTheme.kt
+++ common/src/commonMain/kotlin/tk/zwander/commonCompose/view/components/CustomMaterialTheme.kt
@@ -1,12 +1,10 @@
 package tk.zwander.commonCompose.view.components
 
-import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import tk.zwander.common.util.BifrostSettings
-import tk.zwander.commonCompose.util.FontMapper
 import tk.zwander.commonCompose.util.rememberThemeInfo
 import tk.zwander.commonCompose.view.LocalUseMicaEffect
 
@@ -22,11 +20,6 @@ fun CustomMaterialTheme(block: @Composable () -> Unit) {
         colorScheme = themeInfo.colors,
     ) {
         CompositionLocalProvider(
-            LocalTextStyle provides LocalTextStyle.current.copy(
-                fontFamily = LocalTextStyle.current.fontFamily?.let {
-                    FontMapper.mapGenericFontFamilyToSpecificFontFamily(it)
-                },
-            ),
             LocalUseMicaEffect provides (useMicaEffect == true),
         ) {
             block()
