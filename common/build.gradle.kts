89df1ba36075f86a6985d090927337a01fe90027
Zachary Wander <zachary.wander@gmail.com>
2024-04-28 11:47:53 -0400

    Remove Java-ObjC bridge dependency

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -114,7 +114,6 @@ kotlin {
                 api(libs.oshi.core)
                 api(libs.window.styler)
                 api(libs.kotlinx.coroutines.swing)
-                api(libs.java.objc.bridge)
                 api(libs.jna)
                 api(libs.jna.platform)
 
