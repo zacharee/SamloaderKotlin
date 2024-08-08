204b9edf9b06ca781e9785180b89b83b070090e0
Zachary Wander <zachary.wander@gmail.com>
2024-08-07 22:54:23 -0400

    Update dependencies and address deprecations

--- common/src/androidAndJvmMain/kotlin/tk/zwander/common/util/I18n.androidAndJvm.kt
+++ common/src/androidAndJvmMain/kotlin/tk/zwander/common/util/I18n.androidAndJvm.kt
@@ -4,6 +4,6 @@ import java.util.Locale
 
 actual object I18n {
     actual fun getCountryNameForCode(code: String): String? {
-        return Locale(androidx.compose.ui.text.intl.Locale.current.toLanguageTag(), code).displayCountry
+        return Locale.of(androidx.compose.ui.text.intl.Locale.current.toLanguageTag(), code).displayCountry
     }
 }
