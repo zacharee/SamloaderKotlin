bd14da906f27231072d33f896e738440ebacd7d7
Zachary Wander <zachary.wander@gmail.com>
2024-08-07 22:55:21 -0400

    Undo addressing deprecation

--- common/src/androidAndJvmMain/kotlin/tk/zwander/common/util/I18n.androidAndJvm.kt
+++ common/src/androidAndJvmMain/kotlin/tk/zwander/common/util/I18n.androidAndJvm.kt
@@ -4,6 +4,7 @@ import java.util.Locale
 
 actual object I18n {
     actual fun getCountryNameForCode(code: String): String? {
-        return Locale.of(androidx.compose.ui.text.intl.Locale.current.toLanguageTag(), code).displayCountry
+        @Suppress("DEPRECATION")
+        return Locale(androidx.compose.ui.text.intl.Locale.current.toLanguageTag(), code).displayCountry
     }
 }
