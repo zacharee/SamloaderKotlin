4a136433bde08abf37ccd96e67613bb3899cc79f
Zachary Wander <zachary.wander@gmail.com>
2025-03-22 14:21:03 -0400

    Include custom VAqua lib

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -35,7 +35,7 @@ kotlin {
             dependencies {
                 implementation(project(":common"))
 
-                implementation("org.violetlib:vaqua:13")
+                implementation(files("lib/VAqua.jar"))
             }
         }
     }
@@ -152,7 +152,9 @@ dependencies {
     linuxAarch64(compose.desktop.linux_arm64)
     linuxAmd64(compose.desktop.linux_x64)
     macAarch64(compose.desktop.macos_arm64)
+    macAarch64(files("lib/VAqua.jar"))
     macAmd64(compose.desktop.macos_x64)
+    macAmd64(files("lib/VAqua.jar"))
     windowsAarch64(compose.desktop.windows_arm64)
     windowsAmd64(compose.desktop.windows_x64)
 }
