1a5671bf51f4abd54dbb6aca9eba82a350ed5726
Zachary Wander <zachary.wander@gmail.com>
2025-06-20 17:38:27 -0400

    Use Microsoft JDK

--- iosApp/ci_scripts/ci_post_clone.sh
+++ iosApp/ci_scripts/ci_post_clone.sh
@@ -7,7 +7,7 @@ brew install cocoapods
 
 curl -s "https://get.sdkman.io" | bash
 . "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk install java 21.0.7-amzn
+sdk install java 21.0.7-ms
 
 cd ../../ && chmod +x gradlew && ./gradlew common:podInstall
 
