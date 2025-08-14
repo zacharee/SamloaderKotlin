a475f299c5e21d979dd141f14c8f373787ede48c
Zachary Wander <zachary.wander@gmail.com>
2025-08-14 10:15:08 -0400

    Show accessory warning for SM-L* models

--- common/src/commonMain/kotlin/tk/zwander/common/util/ModelUtils.kt
+++ common/src/commonMain/kotlin/tk/zwander/common/util/ModelUtils.kt
@@ -1,4 +1,4 @@
 package tk.zwander.common.util
 
 val String?.isAccessoryModel: Boolean
-    get() = this?.startsWith("SM-R") == true
+    get() = this?.startsWith("SM-R") == true || this?.startsWith("SM-L") == true
