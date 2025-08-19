91507a370c9d1f01340b424a33bcb5e3b18f41f5
Zachary Wander <zachary.wander@gmail.com>
2025-08-19 10:54:24 -0400

    Update dependencies and fix build

--- common/src/iosMain/kotlin/Main.ios.kt
+++ common/src/iosMain/kotlin/Main.ios.kt
@@ -1,20 +1,9 @@
 @file:Suppress("FunctionName", "unused")
 
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.WindowInsetsSides
-import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.displayCutout
 import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.layout.union
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.InternalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.uikit.InterfaceOrientation
-import androidx.compose.ui.uikit.LocalInterfaceOrientation
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeUIViewController
 import platform.UIKit.UIViewController
 import tk.zwander.commonCompose.MainView
@@ -23,8 +12,6 @@ import kotlin.time.ExperimentalTime
 @OptIn(InternalComposeUiApi::class, ExperimentalTime::class)
 fun MainViewController(): UIViewController {
     return ComposeUIViewController {
-        val orientation = LocalInterfaceOrientation.current
-
         MainView(
             modifier = Modifier
                 .imePadding()
@@ -32,17 +19,3 @@ fun MainViewController(): UIViewController {
         )
     }
 }
-
-val displayBottom: WindowInsets
-    @Composable
-    @OptIn(InternalComposeUiApi::class)
-    get() {
-        val size = 16.dp
-
-        return when (LocalInterfaceOrientation.current) {
-            InterfaceOrientation.Portrait -> WindowInsets(bottom = size)
-            InterfaceOrientation.PortraitUpsideDown -> WindowInsets(top = size)
-            InterfaceOrientation.LandscapeLeft -> WindowInsets(left = size)
-            InterfaceOrientation.LandscapeRight -> WindowInsets(right = size)
-        }
-    }
